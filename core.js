af8505f1f97bd0633583338c52ef855bbdd425a9
pmh-only <opensource@pmh.codes>
2023-06-05 11:23:17 +0900

    fix: don't use base64

--- core.js
+++ core.js
@@ -2,11 +2,11 @@ const socket = new WebSocket('wss://chatsolution.shutupandtakemy.codes')
 
 window.__core = {
   sendBroadcast: (data) =>
-    socket.send(btoa(JSON.stringify(data))),
+    socket.send(JSON.stringify(data)),
 
   onBroadcast: (fn = () => {}) =>
     socket.addEventListener('message', (event) =>
-      fn(JSON.parse(atob(event.data)))),
+      fn(JSON.parse(event.data))),
 
   onConnect: (fn = () => {}) =>
     socket.addEventListener('open', fn)
